0c432721c5cfb5eccc3e3762d35a8489d2140c82
thatt10 <thattshini10@gmail.com>
2021-01-25 12:41:27 +0800

    report UI

--- lib/pages/reportMainPage.dart
+++ lib/pages/reportMainPage.dart
@@ -6,7 +6,9 @@ import 'package:mobilesda3/pages/sponsorReport.dart';
 class ReportMainPage extends StatelessWidget with NavigationStates {
  @override
   Widget build(BuildContext context){
-    return Scaffold(appBar: AppBar(
+    return Scaffold(
+      backgroundColor: Colors.blue[100],
+      appBar: AppBar(
       iconTheme: IconThemeData(
         color: Colors.white, 
   ),
@@ -22,29 +24,37 @@ class ReportMainPage extends StatelessWidget with NavigationStates {
 Widget getListView(BuildContext context){
   var listView = ListView(
     children: <Widget> [
+
+        Card(
+        margin: const EdgeInsets.symmetric(),
+        child: Image(image: NetworkImage('https://builtsurvey.utm.my/wp-content/uploads/2019/04/fabu.jpg'),)
+      ),
+
       Card(
+        margin: const EdgeInsets.symmetric(vertical: 4.0),
         child: ListTile(
         onTap: () {
           Navigator.push(context, MaterialPageRoute(builder: (context) => ReportPage(),
           ));
         } ,
-        leading: Icon(Icons.analytics_outlined),
+        leading: Icon(Icons.analytics_outlined,color: Colors.blue.shade400),
         title: Text("Alumni Batch Report"),
         subtitle: Text("Report on number of alumni per batch"),
-        trailing: Icon(Icons.arrow_forward_ios_rounded),
+        trailing: Icon(Icons.arrow_forward_ios_rounded,color: Colors.blue.shade400),
         ),
       ),
 
       Card(
+        margin: const EdgeInsets.symmetric(),
         child: ListTile(
         onTap: () {
           Navigator.push(context, MaterialPageRoute(builder: (context) => SponsorReport(),
           ));
         } ,
-        leading: Icon(Icons.analytics_outlined),
+        leading: Icon(Icons.analytics_outlined,color: Colors.blue.shade400),
         title: Text("Sponsorship Report"),
-        subtitle: Text("Report on amount and sponsors yearly"),
-        trailing: Icon(Icons.arrow_forward_ios_rounded),
+        subtitle: Text("Report on sponsor amount yearly"),
+        trailing: Icon(Icons.arrow_forward_ios_rounded,color: Colors.blue.shade400),
        ),
       ),
     ],
